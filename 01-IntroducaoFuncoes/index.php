e4a5cfb8840dd8e29ca4c369a261d84f5286c1ea
Anderson <99144364+AndersonMGarcia@users.noreply.github.com>
2022-03-29 20:55:03 -0300

    Intrudução

---
+++ 01-IntroducaoFuncoes/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    //Função que vai somar...
+    // -Numero 1
+    // -Numero 2
+
+    // Internamente:
+    //- Pegar o Numero 1 e Adicionar o Número 2
+
+    // Resultado:
+    // A soma do Numero 1 e Numero 2
+    
